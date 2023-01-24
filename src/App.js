fa03a5e7b2af7a64d256de2f9571f6f47a3f5420
AnjaliKunwar <Anjalirlkunwar@gmail.com>
2023-01-24 13:14:06 +0545

    useful-website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Button from './Button'
+const App=()=>{
+  return<div>
+    <h1> Welcome To my website</h1>
+    <h3> which website do you want to open?</h3>
+    <Button name="google"link="https://www.google.com/"/>
+    <Button name="Facebook"link="https://www.facebook.com/"/>
+    <Button name="YouTube"link="https://www.youtube.com/"/>
+    <br></br>
+    <Button name="Udemy"link="https://www.udemy.com/home/my-courses/lists/"/>
+    <Button name="W3school"link="https://www.w3schools.com/"/>
+  </div> }
+
+ export default App;
